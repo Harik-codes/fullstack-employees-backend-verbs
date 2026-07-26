3f33b4a2b34f93cdc6f37676deaa003dfc38c1e0
Aromal <harikaromal@gmail.com>
2026-07-25 21:14:41 -0500

    final commit

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +7,42 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  await createEmployee({ name: "john", birthday: "2008-07-09", salary: 40000 });
+  await createEmployee({ name: "leo", birthday: "2008-07-09", salary: 40000 });
+  await createEmployee({
+    name: "balingrad",
+    birthday: "2008-07-09",
+    salary: 40000,
+  });
+  await createEmployee({
+    name: "cheto",
+    birthday: "2008-07-09",
+    salary: 40000,
+  });
+  await createEmployee({
+    name: "meila",
+    birthday: "2008-07-09",
+    salary: 40000,
+  });
+  await createEmployee({
+    name: "mikasa",
+    birthday: "2008-07-09",
+    salary: 40000,
+  });
+  await createEmployee({ name: "los", birthday: "2008-07-09", salary: 40000 });
+  await createEmployee({
+    name: "canvas",
+    birthday: "2008-07-09",
+    salary: 40000,
+  });
+  await createEmployee({
+    name: "youla",
+    birthday: "2008-07-09",
+    salary: 40000,
+  });
+  await createEmployee({
+    name: "dansel",
+    birthday: "2008-07-09",
+    salary: 40000,
+  });
 }
